efbfc16f8df0fb8082b8ef7ba39c471f86d5d5bc
Kulakov <prokulakovp@gmail.com>
2026-07-22 15:38:55 +0500

    Update async_base.py
    Decorator check_time integration in main script

--- async_base.py
+++ async_base.py
@@ -1,7 +1,17 @@
 import aiohttp
 import asyncio
 import aiosqlite
-from decorators import check_time
+import time
+
+def check_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Функция {func.__name__} выполнилась за {end - start:.4f} секунд")
+        return result
+    return wrapper
+
 
 DB_PATH = 'liveinternet.db'
 
